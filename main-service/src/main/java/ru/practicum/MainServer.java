c0c556a59d269eb1366664f2be992fde445aaea1
Egor <e.onator@yandex.ru>
2023-02-24 18:08:03 +0300

    add: added db for stat-server

--- main-service/src/main/java/ru/practicum/MainServer.java
+++ main-service/src/main/java/ru/practicum/MainServer.java
@@ -1,10 +1,10 @@
-package ru.practicum.server;
+package ru.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShareItServer {
+public class MainServer {
 
     public static void main(String[] args) {
         SpringApplication.run(MainServer.class, args);
